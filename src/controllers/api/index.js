98353114e81a1f8b42f78f6e2ac26aa53d3c8b66
gingus55 <gingus55@hotmail.com>
2022-01-28 11:53:29 +0000

    api added

---
+++ src/controllers/api/index.js
@@ -0,0 +1,7 @@
+const { getPayloadWithValidFieldsOnly } = require("../../helpers");
+
+const createBlog = () => {};
+
+const createComment = () => {};
+
+module.exports = { createBlog, createComment };
